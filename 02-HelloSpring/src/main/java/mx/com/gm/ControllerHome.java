7f59d7b16f7c745c729d8c282e254dcc0b9b10b1
Richardson94 <richcori94@gmail.com>
2023-06-29 13:22:00 -0400

    spring class 02

--- 02-HelloSpring/src/main/java/mx/com/gm/ControllerHome.java
+++ 02-HelloSpring/src/main/java/mx/com/gm/ControllerHome.java
@@ -1,14 +1,17 @@
 
 package mx.com.gm;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class ControllerHome {
    
     @GetMapping("/")
     public String Home(){
-        return "Hello World from spring!!";
+        log.info("executing the controller Rest");
+        return "Hello World from springBoot";
     }
 }
