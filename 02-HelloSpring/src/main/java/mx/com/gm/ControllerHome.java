8797a6ebcdc31be507a7ba3cc1a2073b8426b4a0
Richardson94 <richcori94@gmail.com>
2023-06-30 23:24:11 -0400

    spring class 04

--- 02-HelloSpring/src/main/java/mx/com/gm/ControllerHome.java
+++ 02-HelloSpring/src/main/java/mx/com/gm/ControllerHome.java
@@ -4,15 +4,18 @@ package mx.com.gm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+
 
 @Controller
 @Slf4j
 public class ControllerHome {
    
     @GetMapping("/")
-    public String Home(){
+    public String Home(Model model){
+        var message = "hello world from Thymeleaf";
         log.info("executing the controller Spring MVC");
+        model.addAttribute("message",message);
         return "index";
     }
 }
