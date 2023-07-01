2aa0850f1453e97bf420b481102cd93295c2fbe9
Richardson94 <richcori94@gmail.com>
2023-06-30 23:37:58 -0400

    spring class 05

--- 02-HelloSpring/src/main/java/mx/com/gm/ControllerHome.java
+++ 02-HelloSpring/src/main/java/mx/com/gm/ControllerHome.java
@@ -2,6 +2,7 @@
 package mx.com.gm;
 
 import lombok.extern.slf4j.Slf4j;
+import mx.com.gm.domain.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
@@ -14,8 +15,14 @@ public class ControllerHome {
     @GetMapping("/")
     public String Home(Model model){
         var message = "hello world from Thymeleaf";
+        var person = new Person();
+        person.setName("Richard");
+        person.setLastName("Cori");
+        person.setEmail("Richard@mail.com");
+        person.setPhone("123456");
         log.info("executing the controller Spring MVC");
         model.addAttribute("message",message);
+        model.addAttribute("Person",person);
         return "index";
     }
 }
