4795c9a87fd4f06808edda57d77cde6e66f51ae5
Richardson94 <richcori94@gmail.com>
2023-07-01 00:08:06 -0400

    spring class 06

--- 02-HelloSpring/src/main/java/mx/com/gm/ControllerHome.java
+++ 02-HelloSpring/src/main/java/mx/com/gm/ControllerHome.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Controller
 @Slf4j
@@ -20,9 +23,21 @@ public class ControllerHome {
         person.setLastName("Cori");
         person.setEmail("Richard@mail.com");
         person.setPhone("123456");
+
         log.info("executing the controller Spring MVC");
+
+        var person2 = new Person();
+        person2.setName("Carla");
+        person2.setLastName("Gomez");
+        person2.setEmail("Car12@mail.com");
+        person2.setPhone("64897");
+
+        List<Person> persons = new ArrayList<>();
+        persons.add(person);
+        persons.add(person2);
         model.addAttribute("message",message);
         model.addAttribute("Person",person);
+        model.addAttribute("PersonsList",persons);
         return "index";
     }
 }
