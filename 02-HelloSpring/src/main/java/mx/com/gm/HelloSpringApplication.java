bf1df3279d4296c76f2081edc5fd2b50a5baaf12
Richardson94 <richcori94@gmail.com>
2023-07-05 22:35:36 -0400

    spring class 07

--- 02-HelloSpring/src/main/java/mx/com/gm/HelloSpringApplication.java
+++ 02-HelloSpring/src/main/java/mx/com/gm/HelloSpringApplication.java
@@ -1,4 +1,5 @@
 package mx.com.gm;
+import java.sql.*;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
+        Connection con;
+        PreparedStatement pst;
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","admin");
+        }
+        catch(ClassNotFoundException ex){
+            ex.printStackTrace();
+        }
+        catch(SQLException ex){
+            ex.printStackTrace();
+        }
 
 		SpringApplication.run(HelloSpringApplication.class, args);
 	}
