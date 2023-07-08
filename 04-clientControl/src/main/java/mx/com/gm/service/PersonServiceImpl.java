e77827e07248a12ab801c81f3fd1ea5d6a28e536
Richardson94 <richcori94@gmail.com>
2023-07-08 11:37:52 -0400

    spring class 14

--- 04-clientControl/src/main/java/mx/com/gm/service/PersonServiceImpl.java
+++ 04-clientControl/src/main/java/mx/com/gm/service/PersonServiceImpl.java
@@ -1,5 +1,6 @@
 package mx.com.gm.service;
 
+import org.springframework.transaction.annotation.Transactional;
 import mx.com.gm.dao.IPersonDao;
 import mx.com.gm.domain.Person;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,22 +15,26 @@ public class PersonServiceImpl implements PersonaService{
     private IPersonDao personDao;
 
     @Override
+    @Transactional(readOnly = true)
     public List<Person> listPersons() {
         return (List<Person>) personDao.findAll();
     }
 
     @Override
+    @Transactional
     public void save(Person person) {
         personDao.save(person);
     }
 
     @Override
+    @Transactional
     public void delete(Person person) {
         personDao.delete(person);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Person findPerson(Person person) {
-       return personDao.findById(person.getIdPerson());
+       return personDao.findById(person.getIdPerson()).orElse(null);
     }
 }
--- 04-clientControl/src/main/java/mx/com/gm/web/ControllerHome.java
+++ 04-clientControl/src/main/java/mx/com/gm/web/ControllerHome.java
@@ -2,25 +2,21 @@
 package mx.com.gm.web;
 
 import lombok.extern.slf4j.Slf4j;
-import mx.com.gm.dao.IPersonDao;
-import mx.com.gm.domain.Person;
+import mx.com.gm.service.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 @Slf4j
 public class ControllerHome {
     @Autowired
-    private IPersonDao personDao;
+    private PersonaService personService;
     @GetMapping("/")
     public String Home(Model model){
-        var persons = personDao.findAll();
+        var persons = personService.listPersons();
 
         return "index";
     }
