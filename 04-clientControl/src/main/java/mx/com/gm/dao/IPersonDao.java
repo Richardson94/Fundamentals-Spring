de2210a0ac85f9bda78f3446cc38415667be03a6
Richardson94 <richcori94@gmail.com>
2023-07-08 11:32:27 -0400

    spring class 13

--- 04-clientControl/src/main/java/mx/com/gm/dao/IPersonDao.java
+++ 04-clientControl/src/main/java/mx/com/gm/dao/IPersonDao.java
@@ -4,6 +4,15 @@ package mx.com.gm.dao;
 import mx.com.gm.domain.Person;
 import org.springframework.data.respository.CrudRepository;
 
+import java.util.List;
+
 public interface IPersonDao extends CrudRepository<Person,Long>{
 
+    List<Person> findAll();
+
+    void save(Person person);
+
+    void delete(Person person);
+
+    Person findById(Long idPerson);
 }
--- 04-clientControl/src/main/java/mx/com/gm/service/PersonServiceImpl.java
+++ 04-clientControl/src/main/java/mx/com/gm/service/PersonServiceImpl.java
@@ -1,6 +1,35 @@
 package mx.com.gm.service;
 
-public class PersonServiceImpl {
+import mx.com.gm.dao.IPersonDao;
+import mx.com.gm.domain.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
 
+@Service
+public class PersonServiceImpl implements PersonaService{
+
+    @Autowired
+    private IPersonDao personDao;
+
+    @Override
+    public List<Person> listPersons() {
+        return (List<Person>) personDao.findAll();
+    }
+
+    @Override
+    public void save(Person person) {
+        personDao.save(person);
+    }
+
+    @Override
+    public void delete(Person person) {
+        personDao.delete(person);
+    }
+
+    @Override
+    public Person findPerson(Person person) {
+       return personDao.findById(person.getIdPerson());
+    }
 }
